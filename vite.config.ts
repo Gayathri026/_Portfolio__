3e6da06c6772469b68aa2a43a5e8d1fd32828944
Gayathri R <157454384+Gayathri026@users.noreply.github.com>
2025-07-09 17:42:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/_Portfolio__"
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
